af831c907d6c8f8edd38e9804402e61c77bbe80e
Aaron Sanchez <saarong12@gmail.com>
2024-10-14 23:04:53 -0500

    Extracted top nav to seperate componentfile.

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,5 +1,6 @@
 // Profile page view.
 import 'package:flutter/material.dart';
+import '../components/top_nav.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -9,22 +10,7 @@ class ProfileView extends StatelessWidget {
     return const Padding(
         padding: EdgeInsets.fromLTRB(10, 60, 10, 0),
         child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(Icons.group_add_outlined),
-                SizedBox(
-                  height: 40,
-                  width: 300,
-                  child: SearchBar(
-                    leading: Icon(Icons.search),
-                  ),
-                ),
-                Icon(Icons.notifications_outlined)
-              ],
-            )
-          ],
+          children: [TopNav(), Text('Profile page here!')],
         ));
   }
 }

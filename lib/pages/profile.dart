f08c35b98814e0d604ff3c884de7f90e00f7df56
Aaron Sanchez <saarong12@gmail.com>
2024-10-18 17:24:32 -0500

    Added static user profile data.

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -2,15 +2,25 @@
 import 'package:flutter/material.dart';
 import '../components/top_nav.dart';
 
+class UserInfo {
+  final String username;
+  final String authId;
+
+  UserInfo({required this.username, required this.authId});
+}
+
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
+  static final UserInfo currentUser =
+      UserInfo(username: 'Chravis', authId: 'a1b2c3');
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-        padding: EdgeInsets.fromLTRB(10, 60, 10, 0),
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(10, 60, 10, 0),
         child: Column(
-          children: [TopNav(), Text('Profile page here!')],
+          children: [const TopNav(), Text(currentUser.username)],
         ));
   }
 }

80c4581a1cae69b75084915d124df0f7be7be23d
Aaron Sanchez <saarong12@gmail.com>
2024-10-20 23:21:14 -0500

    Added profile Avatar and username to the profile page.
    Next update will be the switch to move between views.

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -4,23 +4,45 @@ import '../components/top_nav.dart';
 
 class UserInfo {
   final String username;
+  final String fullName;
   final String authId;
 
-  UserInfo({required this.username, required this.authId});
+  UserInfo(
+      {required this.username, required this.fullName, required this.authId});
 }
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
-  static final UserInfo currentUser =
-      UserInfo(username: 'Chravis', authId: 'a1b2c3');
+  static final UserInfo currentUser = UserInfo(
+      username: 'JohnDoe', fullName: 'Johknee Dough', authId: 'a1b2c3');
 
   @override
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.fromLTRB(10, 60, 10, 0),
         child: Column(
-          children: [const TopNav(), Text(currentUser.username)],
+          children: [
+            const TopNav(),
+            const SizedBox(
+              height: 100,
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.grey[300],
+              radius: 50,
+              child: const Text(
+                'JD',
+                style: TextStyle(fontSize: 40),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              currentUser.fullName,
+              style: const TextStyle(fontSize: 18),
+            )
+          ],
         ));
   }
 }
